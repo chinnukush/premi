6c72d611aae6c3475feb9931689397c310ecb227
chinnukush <chinnukushal123@gmail.com>
2025-08-11 22:17:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -34,8 +34,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=START_MSG.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
-                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
+                [InlineKeyboardButton("ᴘʀᴇᴍɪᴜᴍ", callback_data='premium')]
             ])
         )
 
