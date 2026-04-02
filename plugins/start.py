161753e82d4b07174c305183bbe5706f8b0f2157
chinnukush <chinnukushal123@gmail.com>
2026-04-02 09:58:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -38,8 +38,7 @@ async def short_url(client: Client, message: Message, base64_string):
         ]
 
         await message.reply_photo(
-            caption=SHORT_MSG,
-            protect_content=True.format(
+            caption=SHORT_MSG.format(
             ),
             reply_markup=InlineKeyboardMarkup(buttons),
                 disable_web_page_preview=True
