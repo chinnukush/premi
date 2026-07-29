a4eb1d1921f811751aa49b5072b002574d55de55
chinnukush <chinnukushal123@gmail.com>
2026-07-29 19:19:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,9 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8305589280:AAFJN1RILEFd-QQCLutcqW
 APP_ID = int(os.environ.get("APP_ID", "15671595")) #Your API ID from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "bb8f36f9c39a24c7f8b2acbc7ea8c60a") #Your API Hash from my.telegram.org
 #--------------------------------------------
+WEBSITE_URL = os.environ.get("WEBSITE_URL", "https://harixmoviez.vercel.app")
+WEBSITE_URL_MODE = (os.environ.get("WEBSITE_URL_MODE", "True") == "True")
+
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002082701021")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "Harikushal") # Owner username without @
